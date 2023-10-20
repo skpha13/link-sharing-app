5b4b308bd9855b35e9d0bb33fe45d11a0e9014c4
Adrian Mincu <61752094+skpha13@users.noreply.github.com>
2023-10-20 17:47:28 +0300

    fix: solved tailwindcss

--- src/App.js
+++ src/App.js
@@ -1,19 +1,12 @@
-// import { useEffect, useState } from 'react';
-// import logo from './logo.svg';
 import './App.css';
-// import Navigation from './routes/navigation/navigation.component';
-// import { Route, Routes } from 'react-router-dom';
-// import { Link } from "react-router-dom";
+import { Route, Routes } from 'react-router-dom';
+import Navigation from './routes/navigation/navigation.component';
 
 function App() {
   return (
-    <div className="flex flex-row bg-black justify-center">
-      <h2 className='text-white'>test</h2>
-      <h2 className="text-white">devlinks</h2>
-      {/* <Link className="nav-link" to="/profile">
-        Profile
-      </Link> */}
-    </div>
+    <Routes>
+      <Route path='/' element={<Navigation />} />
+    </Routes>
   );
 }
 
--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -3,10 +3,16 @@ const Navigation = () => {
 
     return(
         <div className="flex flex-row bg-black">
-        <h2>devlinks</h2>
-        <Link className="nav-link" to="/profile">
+        <h2>DevLinks</h2>
+
+        <Link to="/links">
+            Links
+        </Link>
+        
+        <Link to="/profile">
             Profile
-          </Link>
+        </Link>
+
         </div>
     )
 }
